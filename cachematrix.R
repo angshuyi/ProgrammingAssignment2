f076ff72a05e2d80a90f4636f85133dfb5cfc528
ANGSHUYI <shuyi.ang@hotmail.com>
2015-06-17 22:39:18 +0800

    R Programming - Assignment 2
    Code Change

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,8 @@
-# This function is used to create a special object that stores a numeric vector and caches its mean
-# makeVector creates a special "vector", which is really a list containing a function to 
-# set the value of the vector
-# get the value of the vector
-# set the value of the mean
-# get the value of the mean
+# This function assumes that the matrix is always invertible.
+# The following function calculates the mean of the special "vector" created with the above function. 
+# It first checks to see if the mean has already been calculated. 
+# If so, it gets the mean from the cache and skips the computation. 
+# Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function
 
 makeCacheMatrix <- function(x = matrix()) {
   # stores the cached value
@@ -23,12 +22,6 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-# This function assumes that the matrix is always invertible.
-# The following function calculates the mean of the special "vector" created with the above function. 
-# It first checks to see if the mean has already been calculated. 
-# If so, it gets the mean from the cache and skips the computation. 
-# Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function
 cacheSolve <- function(x, ...) {
   
   inv <- x$getinverse()
